a1c339d5bb0b2be87d75acc4fa3035eb5fdca87e
Jordirdp <rodriguez.depablo@gmail.com>
2023-12-28 19:47:50 +0100

    Ejercicio 61

---
+++ Ejercicios/#61-El generador pseudaleatorio.py
@@ -0,0 +1,20 @@
+"""
+Crea un generador de números pseudoaleatorios entre 0 y 100.
+- No puedes usar ninguna función "random" (o semejante) del
+  lenguaje de programación seleccionado.
+
+Es más complicado de lo que parece...
+"""
+
+from datetime import *
+
+def generador ():
+    hora = datetime.now()
+    seconds= int(hora.strftime('%f'))
+    random = int(seconds/10000)
+    print ("El nº es = ", random)
+
+input ("pulsa [intro] cuando quieras generar un numero aleatorio: ")
+generador()
+
+
